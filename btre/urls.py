16dd360d23f6e2c7d95023b34db5f3677f5b8733
Kapre1999 <47268943+Kapre1999@users.noreply.github.com>
2019-12-30 16:57:27 +0530

    Update urls.py

--- btre/urls.py
+++ btre/urls.py
@@ -19,7 +19,7 @@ from django.urls import path,include
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('jaihind/btre/user/admin/panel/', admin.site.urls),
     path('',include('pages.urls')),
     path('listings/',include('listings.urls')),
     path('accounts/',include('accounts.urls')),
